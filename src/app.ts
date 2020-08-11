0d639d7fd2aba3c6c605c0249a20faa355e71f27
sochacki111 <michalsochacki111@gmail.com>
2020-08-11 23:14:53 +0200

    Setup eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 // Create a new express app instance
 const app: express.Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello World!');
 });
 
 export default app;
