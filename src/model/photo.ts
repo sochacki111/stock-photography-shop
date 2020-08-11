953955ba909075c79019cbc32f6d3c14cb3fd5fc
sochacki111 <michalsochacki111@gmail.com>
2020-08-11 20:25:46 +0200

    Basic photo model

---
+++ src/model/photo.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface PhotoInterface extends Document {
+  title: string;
+  author: string;
+  keywords: string[];
+  url: string;
+}
+
+const PhotoSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  keywords: { type: [String], required: true },
+  url: { type: String, required: true },
+});
+
+const Photo = mongoose.model<PhotoInterface>("Photo", PhotoSchema);
+
+export default Photo;
