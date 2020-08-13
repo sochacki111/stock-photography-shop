aa00aea492320885cd8b8ad3830bd9457e0ab5d5
sochacki111 <michalsochacki111@gmail.com>
2020-08-13 21:05:02 +0200

    Setup env vars, logger, mongo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,36 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import logger from './util/logger';
+import { MONGODB_URI } from './util/secrets';
 
 // Create a new express app instance
 const app: express.Application = express();
+
+// Connect to MongoDB
+const mongoUrl = String(MONGODB_URI);
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err) => {
+    logger.debug(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
+    process.exit();
+  });
+
+// Express configuration
+app.set('port', process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello World!');
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,9 @@
 import { Server } from 'http';
+import logger from '../util/logger';
 import app from '../app';
 
 const server: Server = app.listen(3000, () => {
-  console.log('App is listening on port 3000!');
+  logger.debug('App is listening on port 3000!');
 });
 
 export default server;
