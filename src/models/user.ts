52b9311aa0cd2f251b462817acef6d34c404aab3
sochacki111 <michalsochacki111@gmail.com>
2020-08-22 17:30:47 +0200

    Update User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,21 +1,28 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import passportLocalMongoose from 'passport-local-mongoose';
+import mongoose, { Schema, Document, HookNextFunction, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-export interface PhotoInterface extends Document {
+export interface IUser extends Document {
   firstName: string;
   lastName: string;
   email: string;
+  password: string;
+  comparePassword: (password: string) => Promise<boolean>;
 }
 
-const UserSchema: Schema = new mongoose.Schema(
+const UserSchema: Schema = new Schema(
   {
-    first_name: String,
-    last_name: String,
+    firstName: String,
+    lastName: String,
     email: {
       type: String,
       unique: true,
+      required: true,
       lowercase: true,
       trim: true
+    },
+    password: {
+      type: String,
+      required: true
     }
   },
   {
@@ -23,8 +30,32 @@ const UserSchema: Schema = new mongoose.Schema(
   }
 );
 
-UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+// mongoose requires it
+// eslint-disable-next-line func-names
+UserSchema.pre<IUser>('save', async function (
+  next: HookNextFunction
+): Promise<HookNextFunction> {
+  // mongoose requires it
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
 
-const User = mongoose.model<PhotoInterface>('User', UserSchema);
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+
+  return next();
+});
+
+// mongoose requires it
+// eslint-disable-next-line func-names
+UserSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model<IUser>('User', UserSchema);
 
 export default User;
