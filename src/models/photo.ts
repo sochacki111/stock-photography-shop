f42f14c134497cd0d0e398df3231c27055f7db57
sochacki111 <michalsochacki111@gmail.com>
2020-09-18 22:20:05 +0200

    refactor

--- src/models/photo.ts
+++ src/models/photo.ts
@@ -7,12 +7,17 @@ export interface IPhoto extends Document {
   url: string;
 }
 
-const PhotoSchema: Schema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  keywords: { type: [String] },
-  url: { type: String, required: true }
-});
+const PhotoSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    keywords: { type: [String] },
+    url: { type: String, required: true }
+  },
+  {
+    timestamps: true // created_at / updated_at
+  }
+);
 
 const Photo = mongoose.model<IPhoto>('Photo', PhotoSchema);
 
