c166efbfd27e325c987aaa650c3692a7db454f29
sochacki111 <michalsochacki111@gmail.com>
2020-11-22 21:43:32 +0100

    Fixed

--- frontend/src/containers/NewPhoto/NewPhoto.tsx
+++ frontend/src/containers/NewPhoto/NewPhoto.tsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import axios from 'axios';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 
 interface IProps {
   token: string;
-  onAddPhoto: (photoData: any, token: any) => any
+  onAddPhoto: (photoData: any, token: any) => any;
 }
 
 interface IState {
   selectedFile: File | null;
   author: string;
   title: string;
+  category: string;
   price: number;
 }
 
@@ -22,6 +23,7 @@ class NewPhoto extends Component<IProps, IState> {
     selectedFile: null,
     author: '',
     title: '',
+    category: '',
     price: 0
   };
 
@@ -39,11 +41,12 @@ class NewPhoto extends Component<IProps, IState> {
       fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
       fd.append('author', this.state.author);
       fd.append('title', this.state.title);
+      fd.append('category', this.state.category);
       fd.append('price', String(this.state.price));
 
-      const photo = {
-        photoData: fd
-      }
+      // const photo = {
+      //   photoData: fd
+      // };
       // TODO Display progress of upload to the user
       // const res = await axios.post('http://localhost:8080/photos', fd, {
       //   onUploadProgress: (progressEvent) => {
@@ -55,7 +58,8 @@ class NewPhoto extends Component<IProps, IState> {
       //   }
       // });
 
-      await this.props.onAddPhoto(photo, this.props.token);
+      // await this.props.onAddPhoto(photo, this.props.token);
+      await this.props.onAddPhoto(fd, this.props.token);
     }
   };
 
@@ -64,36 +68,60 @@ class NewPhoto extends Component<IProps, IState> {
       <div>
         <h1>Add a Photo</h1>
         <form>
-          <label>File</label>
-          <input type="file" onChange={this.fileSelectedHandler} />
+          <ul>
+            <li>
+              <label>File</label>
+              <input type="file" onChange={this.fileSelectedHandler} />
+            </li>
+            <li>
+              <label>Author</label>
+              <input
+                type="text"
+                value={this.state.author}
+                onChange={(event) =>
+                  this.setState({ author: event.target.value })
+                }
+              />
+            </li>
+            <li>
+              <label>Title</label>
+              <input
+                type="text"
+                value={this.state.title}
+                onChange={(event) =>
+                  this.setState({ title: event.target.value })
+                }
+              />
+            </li>
+            <li>
+              <label>Price</label>
+              <input
+                type="number"
+                value={this.state.price}
+                onChange={(event) =>
+                  this.setState({ price: Number(event.target.value) })
+                }
+              />
+            </li>
+            <li>
+              {/* <select name="sortOrder" onChange={this.categoryHandler}> */}
+              <label>Category</label>
+              <select
+                name="sortOrder"
+                onChange={(event) =>
+                  this.setState({ category: event.target.value })
+                }
+              >
+                <option value="">None</option>
+                <option value="Fashion">Fashion</option>
+                <option value="Aerial">Aerial</option>
+                <option value="Travel">Travel</option>
+                <option value="Animals">Animals</option>
+              </select>
+            </li>
+            <button onClick={this.fileUploadHandler}>Upload</button>
+          </ul>
         </form>
-        <form>
-          <label>Author</label>
-          <input
-            type="text"
-            value={this.state.author}
-            onChange={(event) => this.setState({ author: event.target.value })}
-          />
-        </form>
-        <form>
-          <label>Title</label>
-          <input
-            type="text"
-            value={this.state.title}
-            onChange={(event) => this.setState({ title: event.target.value })}
-          />
-        </form>
-        <form>
-          <label>Price</label>
-          <input
-            type="number"
-            value={this.state.price}
-            onChange={(event) =>
-              this.setState({ price: Number(event.target.value) })
-            }
-          />
-        </form>
-        <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
     );
   }
@@ -101,18 +129,18 @@ class NewPhoto extends Component<IProps, IState> {
 
 const mapStateToProps = (state: any) => {
   return {
-      // receivedProp: stateFromRedux
-      loading: state.addPhoto.loading,
-      token: state.auth.token
-  }
+    token: state.auth.token
+  };
 };
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
-      onAddPhoto: (photoData: any, token: any) => dispatch(actions.addPhoto(photoData, token))
+    onAddPhoto: (photoData: any, token: any) =>
+      dispatch(actions.addPhoto(photoData, token))
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(NewPhoto, axios));
-
-// export default NewPhoto;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(NewPhoto, axios));
